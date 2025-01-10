03f866df4518c8df49b087b4be5bd9489f6160bc
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-01-10 02:29:32 +0200

    user routes tested

--- server/models/users.js
+++ server/models/users.js
@@ -5,12 +5,6 @@ const User = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
-  subscription: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Course',
-    },
-  ],
 },
 { timestamps: true },
 { versionKey: false });
--- server/tests/routs/userRoutes.test.js
+++ server/tests/routs/userRoutes.test.js
@@ -3,136 +3,138 @@ import { app } from '../../app.js';
 import User from '../../models/users.js';
 
 const newUser = {
-  name: "ahmed",
-  email: "rrazcr@gmail.com",
-  password: "ALxafrica123@",
-  role: "instructor",
+  name: 'ahmed',
+  email: 'rrazcr@gmail.com',
+  password: 'ALxafrica123@',
+  role: 'instructor',
 };
 
-const ExistsUser = {
-  password: "test2025@",
-  email: "alxtest@gmail.com"
-}
-
-describe('User Routs', () => {
-  
-  describe('POST /api/user/register', () => {
-    afterEach(async () => {
-      // To delete the user created in database 
-      await User.deleteMany(newUser);
-    });
-    it('should successfully register', async () => {
-      const result = await supertest(app).post('/api/user/register')
-        .send(newUser);
-      expect(result.status).toBe(201);
-      expect(result.body.success).toBe(true);
-      expect(result.body.message).toBe('User created successfully!');
-    });
-    describe('Invalid password', () => {
-      it('should fail due to short password', async () => {
-        const result = await supertest(app).post('/api/user/register')
-        .send({
-          name: newUser.name,
-          email: newUser.email,
-          password: "short",
-        });
-        expect(result.status).toBe(400);
-        expect(result.body.success).toBe(false);
-        expect(result.body.error).toBe('Password must be between 6 and 20 characters long!');
-      })
-
-      it('should fail due to Unused special character', async () => {
-        const result = await supertest(app).post('/api/user/register')
-        .send({
-          name: newUser.name,
-          email: newUser.email,
-          password: "ShortPassword",
-        });
-        expect(result.status).toBe(400);
-        expect(result.body.success).toBe(false);
-        expect(result.body.error).toBe('Password must contain at least one special character!');
-      });
-    });
-    it('should fail already registerd email', async () => {
-      const result = await supertest(app).post('/api/user/register')
-        .send({
-          name: newUser.name,
-          email: 'ahmeddd@gmail.com', // already exists in database
-          password: newUser.password,
-        });
-        expect(result.status).toBe(409);
-        expect(result.body.success).toBe(false);
-        expect(result.body.error).toBe('This email already exists!');
-    });
+describe('post /api/user/register', () => {
+  afterAll(async () => {
+    // To delete the user created in database
+    await User.deleteMany({ email: newUser.email });
   });
-
-  describe('POST /api/user/login', () => {
-    it('should successfully login', async () => {
-      const result = await supertest(app).post('/api/user/login')
+  it('should successfully register', async () => {
+    expect.assertions(3);
+    const result = await supertest(app).post('/api/user/register')
+      .send(newUser);
+    expect(result.status).toBe(201);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('User created successfully!');
+  });
+  // Invalid password
+  describe('invalid password', () => {
+    it('should fail due to short password', async () => {
+      expect.assertions(3);
+      const result = await supertest(app).post('/api/user/register')
         .send({
-          email: ExistsUser.email, // already exists in database
-          password: ExistsUser.password,
+          name: newUser.name,
+          email: newUser.email,
+          password: 'short',
         });
-        expect(result.status).toBe(200);
-        expect(result.body.success).toBe(true);
-        expect(result.body.message).toBe('Logged in successfully.');
-    });
-    describe('Missing Parameter', () => {
-        it('should fail Missing password', async () => {
-        const result = await supertest(app).post('/api/user/login')
-          .send({
-            email: ExistsUser.email,
-          });
-          expect(result.status).toBe(400);
-          expect(result.body.success).toBe(false);
-          expect(result.body.error).toBe('Password is missing!');
-        });
-        it('should fail Missing email', async () => {
-          const result = await supertest(app).post('/api/user/login')
-            .send({
-              password: ExistsUser.password,
-            });
-            expect(result.status).toBe(400);
-            expect(result.body.success).toBe(false);
-            expect(result.body.error).toBe('Email is invalid');
-        });
-      });
-    });
-
-  describe('GET /api/user/me', () => {
-    
-    let token; 
-
-    // befor get the routes me for info you should first login
-    beforeAll( async () => {
-      const loginresponse = await supertest(app)
-        .post('/api/user/login')
-        .send(ExistsUser);
-      token = loginresponse.body.token; 
-    });
-
-    it('should successfully retrive info', async () => {
-      const result = await supertest(app)
-        .get('/api/user/me')
-        .set('Authorization', `Bearer ${token}`); // to check when token exists
-      expect(result.status).toBe(200);
-      expect(result.body.success).toBe(true);
-      expect(result.body.message).toBe('successfully retrived');
-    });
-    it('should fail due to not exists token', async () => {
-      const result = await supertest(app)
-        .get('/api/user/me')
-      expect(result.status).toBe(401);
-      expect(result.body.success).toBe(false);
-      expect(result.body.error).toBe('Access denied, no token provided');
-    });
-    it('should fail due to invalid token', async () => {
-      const result = await supertest(app)
-        .get('/api/user/me')
-        .set('Authorization', `Bearer InValid token`);
       expect(result.status).toBe(400);
       expect(result.body.success).toBe(false);
-      expect(result.body.error).toBe('Invalid token');
+      expect(result.body.error).toBe('Password must be between 6 and 20 characters long!');
     });
-  })    
+    // speacial password character and length check
+    it('should fail due to Unused special character', async () => {
+      expect.assertions(3);
+      const result = await supertest(app).post('/api/user/register')
+        .send({
+          name: newUser.name,
+          email: newUser.email,
+          password: 'ShortPassword',
+        });
+      expect(result.status).toBe(400);
+      expect(result.body.success).toBe(false);
+      expect(result.body.error).toBe('Password must contain at least one special character!');
+    });
+  });
+  it('should fail already registerd email', async () => {
+    expect.assertions(3);
+    const result = await supertest(app).post('/api/user/register')
+      .send(newUser);
+    expect(result.status).toBe(409);
+    expect(result.body.success).toBe(false);
+    expect(result.body.error).toBe('This email already exists!');
+  });
+});
+
+describe('post /api/user/login', () => {
+  beforeAll(async () => {
+    await supertest(app).post('/api/user/register').send(newUser);
+  });
+  afterAll(async () => {
+    await User.deleteMany({ email: newUser.email });
+  });
+  it('should successfully login', async () => {
+    expect.assertions(3);
+    const result = await supertest(app).post('/api/user/login')
+      .send({
+        email: newUser.email, // already exists in database
+        password: newUser.password,
+      });
+    expect(result.status).toBe(200);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('Logged in successfully.');
+  });
+  describe('missing Parameter', () => {
+    it('should fail Missing password', async () => {
+      expect.assertions(3);
+      const result = await supertest(app).post('/api/user/login')
+        .send({
+          email: newUser.email,
+        });
+      expect(result.status).toBe(400);
+      expect(result.body.success).toBe(false);
+      expect(result.body.error).toBe('Password is missing!');
+    });
+    it('should fail Missing email', async () => {
+      expect.assertions(3);
+      const result = await supertest(app).post('/api/user/login')
+        .send({
+          password: newUser.password,
+        });
+      expect(result.status).toBe(400);
+      expect(result.body.success).toBe(false);
+      expect(result.body.error).toBe('Email is invalid');
+    });
+  });
+});
+describe('get /api/user/me', () => {
+  let token;
+  beforeEach(async () => {
+    await supertest(app).post('/api/user/register').send(newUser);
+    const loginforNow = await supertest(app).post('/api/user/login').send({ email: newUser.email, password: newUser.password });
+    token = loginforNow.body.token;
+  });
+  afterEach(async () => {
+    await User.deleteMany({ email: newUser.email });
+  });
+  it('should successfully retrive info', async () => {
+    expect.assertions(3);
+    const result = await supertest(app)
+      .get('/api/user/me')
+      .set('Authorization', `Bearer ${token}`); // to check when token exists
+    expect(result.status).toBe(200);
+    expect(result.body.success).toBe(true);
+    expect(result.body.message).toBe('successfully retrived');
+  });
+  it('should fail due to not exists token', async () => {
+    expect.assertions(3);
+    const result = await supertest(app)
+      .get('/api/user/me');
+    expect(result.status).toBe(401);
+    expect(result.body.success).toBe(false);
+    expect(result.body.error).toBe('Access denied, no token provided');
+  });
+  it('should fail due to invalid token', async () => {
+    expect.assertions(3);
+    const result = await supertest(app)
+      .get('/api/user/me')
+      .set('Authorization', 'Bearer InValid token');
+    expect(result.status).toBe(400);
+    expect(result.body.success).toBe(false);
+    expect(result.body.error).toBe('Invalid token');
+  });
 });
