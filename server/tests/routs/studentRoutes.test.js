be05456cedfd1342fd2c6bd8e8c180bf7159d87b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-01-10 03:40:34 +0200

    test enroll route

--- server/tests/routs/studentRoutes.test.js
+++ server/tests/routs/studentRoutes.test.js
@@ -6,67 +6,79 @@ import { Content, Courses } from '../../models/courses.js';
 import progress from '../../models/progress.js';
 
 
-const ExistsUser = {
-    name: "ahmed",
-    email: "zxc@gmail.com",
-    password: "ALxafrica123@",
-    role: "student",
+const studentUser = {
+    name: 'ahmed',
+    email: 'zxc@gmail.com',
+    password: 'ALxafrica123@',
+    role: 'student',
 };
 const instuctUser = {
-    name: "ahmed",
-    email: "ali@gmail.com",
-    password: "ALxafrica123@",
-    role: "instructore",
+    name: 'ahmed',
+    email: 'ali@gmail.com',
+    password: 'ALxafrica123@',
+    role: 'instructor',
 };
 
-describe('Student Routes', () => {
+const courseCreate = {
+  title: 'test',
+  description: 'test course',
+  category: 'tesing',
+  difficulty: 'hard',
+}
 
+describe('student Routes', () => {
+  let studReg;
+  let register;
+  let getCourse;
   let token;
-  let course;  
-  // create a course
-  beforeAll( async () => {
-    
-    await User.create(ExistsUser);
-    await User.create(instuctUser);
 
-    const insLogin =await supertest(app)
-      .post('/api/user/login')
-      .send({
-        email: instuctUser.email,
-        password: instuctUser.password,
-      });
+  beforeAll(async () => {
+    register = await supertest(app).post('/api/user/register').send(instuctUser);
 
-    const loginResponse = await supertest(app)
-      .post('/api/user/login')
-      .send({
-        email: ExistsUser.email,
-        password: ExistsUser.password,
-      });  
-    token = loginResponse.body.token;
-    await supertest(app).post('/api/instructor/')
-    course = await Courses.create({
-      "title": "Python",
-      "category": "programming",     	
-      "difficulty": "easy",
-      "instructorId": insLogin.par,
-      "description": "etc....",
+    studReg = await supertest(app).post('/api/user/register').send(studentUser);
+
+    const loginuser = await supertest(app).post('/api/user/login').send({ email: studentUser.email, password: studentUser.password });
+
+    token = loginuser.body.token;
+
+    getCourse = await Courses.create({
+      title: courseCreate.title,
+      description: courseCreate.description,
+      category: courseCreate.category,
+      difficulty: courseCreate.difficulty,
+      instructorId: register.body.user.id,
     });
+  
   });
-
-  afterAll( async () => {
-    await User.deleteMany(ExistsUser)
-  });
-
-  describe('POST /api/student/enroll', () => {
+  afterAll(async () => {
+    await enrollments.deleteMany({ userId: studReg.body.user.id });
+    await User.deleteMany({ email: studentUser.email });
+    await User.deleteMany({ email: instuctUser.email });
+    await Courses.deleteMany({ title: courseCreate.title });
+  })
+  describe('post /api/student/enroll', () => {
 
     it('should successfully enroll in course', async () => {
+      expect.assertions(2);
+      console.log(getCourse);
       const result = await supertest(app).post('/api/student/enroll')
         .set('Authorization', `Bearer ${token}`)
         .send({
-          courseId: "course._id",
+          courseId: getCourse._id,
+          userId: studReg.body.user.id,
         });
         expect(result.status).toBe(200);
         expect(result.body.message).toBe('Successfully enrolled in the course');
     });
+    it('should fail due to missing paramter', async () => {
+      expect.assertions(2);
+      const result = await supertest(app).post('/api/student/enroll')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          userId: studReg.body.user.id,
+        });
+      expect(result.status).toBe(401);
+      expect(result.body.error).toBe('Missing courseId');
+    })
   });
 });
